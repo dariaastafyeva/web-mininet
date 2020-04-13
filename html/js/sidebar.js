ff0f6e193a688d1ffe866e518fdf3904cf5edc23
dariatikhonova <mermalya17@gmail.com>
2020-04-13 20:21:16 +0300

    can move host

--- html/js/sidebar.js
+++ html/js/sidebar.js
@@ -11,31 +11,31 @@ function closeNav() {
 }
 
 function addNode(type){
+    hostCount = switchCount = contCount = 0;
+
     if(type == 'host'){
         var img = $('<img id="host-img">'); //Equivalent: $(document.createElement('img'))
         img.attr({
            src: "/images/host.png",
         });
         img.appendTo('#main');
-        console.log(img)
-        console.log(img.get())
-        ball = $( "#host-img" ).get( 0 );
-        console.log(ball)
-        ball.onmousedown = function(event) {
-            let shiftX = event.clientX - ball.getBoundingClientRect().left;
-            let shiftY = event.clientY - ball.getBoundingClientRect().top;
+        hostCount += 1;
+        let node = $( "#host-img" ).get( 0 );
+        node.onmousedown = function(event) {
+            let shiftX = event.clientX - node.getBoundingClientRect().left;
+            let shiftY = event.clientY - node.getBoundingClientRect().top;
 
-            ball.style.position = 'absolute';
-            ball.style.zIndex = 1000;
-            document.body.append(ball);
+            node.style.position = 'absolute';
+            node.style.zIndex = 1000;
+            document.body.append(node);
 
             moveAt(event.pageX, event.pageY);
 
             // переносит мяч на координаты (pageX, pageY),
             // дополнительно учитывая изначальный сдвиг относительно указателя мыши
             function moveAt(pageX, pageY) {
-            ball.style.left = pageX - shiftX + 'px';
-            ball.style.top = pageY - shiftY + 'px';
+            node.style.left = pageX - shiftX + 'px';
+            node.style.top = pageY - shiftY + 'px';
             }
 
             function onMouseMove(event) {
@@ -46,15 +46,98 @@ function addNode(type){
             document.addEventListener('mousemove', onMouseMove);
 
             // отпустить мяч, удалить ненужные обработчики
-            ball.onmouseup = function() {
+            node.onmouseup = function() {
             document.removeEventListener('mousemove', onMouseMove);
-            ball.onmouseup = null;
+            node.onmouseup = null;
             };
-            ball.ondragstart = function() {
+            node.ondragstart = function() {
             return false;
             };
             };
-        }
-    console.log(ball)
+    }else if(type == 'switch'){
+        var img = $('<img id="switch-img">'); //Equivalent: $(document.createElement('img'))
+        img.attr({
+           src: "/images/switch.png",
+        });
+        img.appendTo('#main');
+        switchCount += 1;
+        let node = $( "#switch-img" ).get( 0 );
+        node.onmousedown = function(event) {
+            let shiftX = event.clientX - node.getBoundingClientRect().left;
+            let shiftY = event.clientY - node.getBoundingClientRect().top;
+
+            node.style.position = 'absolute';
+            node.style.zIndex = 1000;
+            document.body.append(node);
+
+            moveAt(event.pageX, event.pageY);
+
+            // переносит мяч на координаты (pageX, pageY),
+            // дополнительно учитывая изначальный сдвиг относительно указателя мыши
+            function moveAt(pageX, pageY) {
+            node.style.left = pageX - shiftX + 'px';
+            node.style.top = pageY - shiftY + 'px';
+            }
+
+            function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+            }
+
+            // передвигаем мяч при событии mousemove
+            document.addEventListener('mousemove', onMouseMove);
+
+            // отпустить мяч, удалить ненужные обработчики
+            node.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            node.onmouseup = null;
+            };
+            node.ondragstart = function() {
+            return false;
+            };
+            };
+    }else if(type == 'controller'){
+        var img = $('<img id="contr-img">'); //Equivalent: $(document.createElement('img'))
+        img.attr({
+           src: "/images/controller.png",
+        });
+        img.appendTo('#main');
+        switchCount += 1;
+        let node = $( "#contr-img" ).get( 0 );
+        node.onmousedown = function(event) {
+            let shiftX = event.clientX - node.getBoundingClientRect().left;
+            let shiftY = event.clientY - node.getBoundingClientRect().top;
+
+            node.style.position = 'absolute';
+            node.style.zIndex = 1000;
+            document.body.append(node);
+
+            moveAt(event.pageX, event.pageY);
+
+            // переносит мяч на координаты (pageX, pageY),
+            // дополнительно учитывая изначальный сдвиг относительно указателя мыши
+            function moveAt(pageX, pageY) {
+            node.style.left = pageX - shiftX + 'px';
+            node.style.top = pageY - shiftY + 'px';
+            }
+
+            function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+            }
+
+            // передвигаем мяч при событии mousemove
+            document.addEventListener('mousemove', onMouseMove);
+
+            // отпустить мяч, удалить ненужные обработчики
+            node.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            node.onmouseup = null;
+            };
+            node.ondragstart = function() {
+            return false;
+            };
+            };
+    }
+            
+    console.log(node)
 }
-//ball = document.getElementById('host-img');
+//node = document.getElementById('host-img');
